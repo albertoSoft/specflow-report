89bf5af5a7da0f4901677bf5614933266af1edea
Ruben Loredo <ruben.loredo33@gmail.com>
2023-06-09 17:07:51 -0400

    first commit

---
+++ TestProject3/Feature1StepDefinitions.cs
@@ -0,0 +1,33 @@
+using RestSharp;
+using System;
+using System.Net;
+using TechTalk.SpecFlow;
+
+namespace TestProject3
+{
+    [Binding]
+    public class Feature1StepDefinitions
+    {
+        RestClient client = new RestClient("http://localhost:3000/");
+        RestRequest request = new RestRequest("posts/{postId}", Method.Get);
+        RestResponse response;
+
+        [Given(@"I have a id with value (.*)")]
+        public void GivenIHaveAIdWithValue(int p0)
+        {
+            request.AddUrlSegment("postId", p0);
+        }
+
+        [When(@"I send a GET request")]
+        public void WhenISendAGETRequest()
+        {
+            response = client.Execute(request);
+        }
+
+        [Then(@"I expected a valid code response")]
+        public void ThenIExpectedAValidCodeResponse()
+        {
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        }
+    }
+}
---
+++ TestProject3/features/GetPosts.feature
@@ -0,0 +1,9 @@
+﻿Feature: Feature1
+
+A short summary of the feature
+
+@positive @smoke @positive @regretion @integration @JIRA-7777
+Scenario: Get By id
+	Given I have a id with value 1
+	When  I send a GET request
+	Then I expected a valid code response
